fc9617f58310603f82becea496bdd2f272c282e7
chris <chrisoquinn.2@gmail.com>
2024-01-16 16:24:45 -0800

    Mostly commenting, and a little bit of properties work

--- src/controller.py
+++ src/controller.py
@@ -15,6 +15,41 @@ from src.types import PyTree
 
 
 class ACT_Controller(Immutable):
+    """
+    A controller for the ACT process.
+
+    This is an ACT controller designed to be
+    setup in the associated builder, and which will accumulate
+    information across act steps for all targeted quantities.
+
+    The class must be initialized with a ACTStates object, and
+    will generally return a new instance of itself whenever it
+    is modified.
+
+    The controller is completely immutable. Once initialized, if a method
+    producing a state change is called, a new controller is returned instead.
+
+    ----- Fields ----
+    probabilities: The cumulative probabilities for each batch element
+    residuals: The residuals on each batch element
+    iterations: The iteration each batch is on, or alternatively where
+    accumulators: The accumulated results
+
+    ----- Properties ----
+
+    halt_threshold: Above this cumulative probability a accumulator is considered halted
+    halted_batches: A bool tensor of batches that are halted. True means halted.
+    is_completely_halted: If every batch element is halted, this is true
+    is_any_halted: If at least one batch element is halted, this is true.
+
+    ----- Methods -----
+
+
+
+
+
+    """
+
 
     # Direct properties
     @property
@@ -41,8 +76,15 @@ class ACT_Controller(Immutable):
     def halted_batches(self)->jnp.ndarray:
         return self.probabilities > self.halt_threshold
 
-    # Helper logic
+    @property
+    def is_completely_halted(self)->bool:
+        return jnp.all(self.halted_batches)
 
+    @property
+    def is_any_halted(self)->bool:
+        return jnp.any(self.halted_batches)
+
+    # Helper logic
     def _process_probabilities(self,
                                halting_probabilities: jnp.ndarray
                                )->Tuple[jnp.ndarray,
@@ -73,7 +115,7 @@ class ACT_Controller(Immutable):
         # are still useful for probability clamping
 
         raw_residuals = 1 - halting_probabilities
-        residuals = jnp.where(newly_halting, raw_residuals, self.state.residuals)
+        residuals = jnp.where(newly_halting, raw_residuals, self.residuals)
 
         # Halting probabilities are clamped to the raw residual entries
         # where they will be halted. This ensures total probability can
@@ -93,6 +135,15 @@ class ACT_Controller(Immutable):
                             update_value: Optional[jnp.ndarray],
                             halting_probabilities: jnp.ndarray
                             ) -> jnp.ndarray:
+        """
+        Performs an update step using an accumulator, halting
+        probabilities, and the update.
+
+        :param accumulator_value: The current accumulator
+        :param update_value: The value to include in the update
+        :param halting_probabilities: The halting probabilities
+        :return: The new accumulator.
+        """
 
         if update_value is None:
             msg = f"""
@@ -110,19 +161,30 @@ class ACT_Controller(Immutable):
             msg = textwrap.dedent(msg)
             raise RuntimeError(msg)
 
-        # Broadcast halting probabilities to match, then weight and add.
+        # We will need the halting probabilities and halting batches tensors
+        # to be broadcastable, from the left, with the accumulator shapes.
+        #
+        # We will often need to unsqueeze on the last dimension to make this
+        # happen. This section accomplishes this.
+        halted_batches = self.halted_batches
         while len(halting_probabilities.shape) < len(accumulator_value.shape):
             halting_probabilities = halting_probabilities[..., None]
-        return accumulator_value + halting_probabilities * update_value
+            halted_batches = halted_batches[..., None]
 
-    # Main logic.
+        # We weight then add. The result is then created into an update, but
+        # only accumulators which have not already reached a halting state get updated.
+
+        new_accumulators = accumulator_value + halting_probabilities * update_value
+        return jnp.where(halted_batches, accumulator_value, new_accumulators)
+
+    # Main loop logic
     def cache_accumulator(self,
                           name: str,
                           item: PyTree
                           )->'ACT_Controller':
         """
         Cache a new group of accumulator pytrees onto a configured
-        entry. Caching will save for the later weight-and-add process.
+        entry. Caching will save the tree for the later weight-and-add process.
 
         :param name: The name of the accumulator to cache
         :param item: What to actually cache
@@ -135,7 +197,7 @@ class ACT_Controller(Immutable):
         if name not in state.updates:
             raise ValueError(f"Accumulator with name {name} was never setup")
         if state.updates[name] != None:
-            raise RuntimeError(f"Accumulator with name {name} was already set")
+            raise RuntimeError(f"Accumulator with name {name} was already set this iteration!")
         update = state.updates.copy()
         update[name] = item
         new_state = state.replace(updates=update)
@@ -176,7 +238,11 @@ class ACT_Controller(Immutable):
                 msg = f"An error occurred regarding accumulator {name}: \n\n"
                 raise RuntimeError(msg) from err
 
-        iterations = self.iterations + 1
+        # We update the iteration count only where we have
+        # not reached the halting state
+
+        new_iterations = self.iterations + 1
+        iterations = jnp.where(self.halted_batches, self.iterations, new_iterations)
 
         # Create the updated state. Return.
 
@@ -193,11 +259,6 @@ class ACT_Controller(Immutable):
         super().__init__()
         self.state = state
         self.lock()
-
-class ACT_Controller:
-    @staticmethod
-    def halt_threshold(state: ACTStates)->float:
-        return 1 - state.epsilon
 
 
 
